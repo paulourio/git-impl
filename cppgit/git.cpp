303d2d43948acfe4c1bf502e03c2ec57e7a532cb
Paulo Roberto Urio <paulourio@gmail.com>
2019-04-19 08:10:04 -0300

    C++ exception handling in init
    Signed-off-by: Paulo Roberto Urio <paulourio@gmail.com>

--- cppgit/git.cpp
+++ cppgit/git.cpp
@@ -1,24 +1,30 @@
+#include <iostream>
 #include <filesystem>
 #include <string>
-#include <stdio.h>
 
 namespace fs = std::filesystem;
 
 int main(int argc, char** argv) {
   using std::string;
+  using std::cin, std::cout, std::cerr, std::endl;
+
   string command(argv[1]);
 
   if (command == "init") {
     auto path = fs::absolute(argv[2]);
     auto git_path = path /= ".git";
 
-    for (auto dirname : {"objects", "refs"}) {
-      fs::create_directories(git_path /= dirname);
-    }
+    try {
+      for (auto dirname : {"objects", "refs"}) {
+        fs::create_directories(git_path /= dirname);
+      }
 
-    printf("Initialized empty git repository in %s\n", git_path.c_str());
+      cerr << "Initialized empty git repository in " << git_path << endl;
+    } catch (std::filesystem::filesystem_error& e) {
+      cerr << "fatal: " << e.what() << endl;
+    }
   } else {
-    fprintf(stderr, "git: '%s' is not a git command.\n", command.c_str());
+    cerr << "git: '" << command << "' is not a git command." << endl;
     exit(1);
   }
   return 0;
